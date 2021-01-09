1343e2b05c6f4ca6cfad23dc1b467d77f193e4d1
Cesar.Villegas.T <cesar@vendamasfacil.com>
2021-01-09 16:25:49 -0500

    Set Cashbox Base.
    - Added method to manage set cashbox base API request.

--- lambdas/models/MySQLDAO.js
+++ lambdas/models/MySQLDAO.js
@@ -154,8 +154,156 @@ class MySQLDAO {
     });
   }
 
-  setCashBoxBase() {
-    return "holaaaaaaaaaaa";
+  setCashBoxBase(denominations_to_add) {
+    const {
+      billete_100000 = 0,
+      billete_50000 = 0,
+      billete_20000 = 0,
+      billete_10000 = 0,
+      billete_5000 = 0,
+      billete_1000 = 0,
+      moneda_1000 = 0,
+      moneda_500 = 0,
+      moneda_200 = 0,
+      moneda_100 = 0,
+      moneda_50 = 0,
+    } = denominations_to_add;
+    let conn = this.connection;
+    return new Promise((resolve, reject) => {
+      conn.beginTransaction(function (err) {
+        if (err) {
+          throw err;
+        }
+        conn.query(
+          `SELECT id, quantity
+          FROM cashbox
+          WHERE quantity > 0
+          ORDER BY id;`,
+          function (error, results, fields) {
+            if (error) {
+              return conn.rollback(function () {
+                conn.end();
+                reject(error);
+              });
+            }
+            let denominations_to_register = { results: results };
+            const denominations = denominations_to_register.results;
+            const size = denominations.length;
+
+            if (size > 0) {
+              return conn.rollback(function () {
+                conn.end();
+                reject("Debe primero limpiar la caja");
+              });
+            }
+
+            let payment =
+              billete_100000 * 100000 +
+              billete_50000 * 50000 +
+              billete_20000 * 20000 +
+              billete_10000 * 10000 +
+              billete_5000 * 5000 +
+              billete_1000 * 1000 +
+              moneda_1000 * 1000 +
+              moneda_500 * 500 +
+              moneda_200 * 200 +
+              moneda_100 * 100 +
+              moneda_50 * 50;
+            let movement_params = {
+              payment: payment,
+              cash_back: 0,
+              operations_id: 2,
+            };
+            conn.query(
+              `INSERT INTO movements SET ?`,
+              movement_params,
+              function (error, results, fields) {
+                if (error) {
+                  return conn.rollback(function () {
+                    conn.end();
+                    reject(error);
+                  });
+                }
+
+                let movement_id = results.insertId;
+                let casbox_query = `UPDATE cashbox SET quantity = CASE id
+                 WHEN 1 THEN ${billete_100000} 
+                 WHEN 2 THEN ${billete_50000}
+                 WHEN 3 THEN ${billete_20000} 
+                 WHEN 4 THEN ${billete_10000}
+                 WHEN 5 THEN ${billete_5000} 
+                 WHEN 6 THEN ${billete_1000}
+                 WHEN 7 THEN ${moneda_1000} 
+                 WHEN 8 THEN ${moneda_500}
+                 WHEN 9 THEN ${moneda_200} 
+                 WHEN 10 THEN ${moneda_100}
+                 WHEN 11 THEN ${moneda_50}
+                 ELSE quantity
+                 END
+                 WHERE id IN(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);`;
+
+                conn.query(casbox_query, function (error, results, fields) {
+                  if (error) {
+                    return conn.rollback(function () {
+                      reject(error);
+                    });
+                  }
+
+                  let insertQuery =
+                    "INSERT INTO movements_details (movements_id, cashbox_id, quantity, detail_operation_id) VALUES";
+
+                  let parameters = "";
+
+                  let denominations_array = [
+                    billete_100000,
+                    billete_50000,
+                    billete_20000,
+                    billete_10000,
+                    billete_5000,
+                    billete_1000,
+                    moneda_1000,
+                    moneda_500,
+                    moneda_200,
+                    moneda_100,
+                    moneda_50,
+                  ];
+
+                  for (let i = 0; i < 11; i++) {
+                    if (denominations_array[i] !== 0) {
+                      parameters += ` (${movement_id}, ${i + 1}, ${
+                        denominations_array[i]
+                      }, 1 ),`;
+                    }
+                  }
+
+                  insertQuery += parameters;
+                  insertQuery = insertQuery.replace(/.$/, ";");
+
+                  conn.query(insertQuery, function (error, results, fields) {
+                    if (error) {
+                      return conn.rollback(function () {
+                        reject(error);
+                        // throw error;
+                      });
+                    }
+
+                    conn.commit(function (err) {
+                      if (err) {
+                        return conn.rollback(function () {
+                          throw err;
+                        });
+                      }
+                      conn.end();
+                      resolve(true);
+                    });
+                  });
+                });
+              }
+            );
+          }
+        );
+      });
+    });
   }
 
   paymentRegister() {
