e186bba5bc0ea6abbc98746d2e21e8b276341d9f
Cesar.Villegas.T <cesar@vendamasfacil.com>
2021-01-10 01:27:46 -0500

    Added get previous cashbox status.
    - Added algorithm to get historical from movements and reconstruct cashbox status on specific date.

--- lambdas/handlers/getPreviousCashBoxStatus.js
+++ lambdas/handlers/getPreviousCashBoxStatus.js
@@ -1,10 +1,49 @@
 const MySQLDAO = require("../models/MySQLDAO");
 
 exports.handler = async (event) => {
+  let requiredDate = event.queryStringParameters.date_required;
+
   let MySQLDAOInstance = new MySQLDAO();
   let result_body = await MySQLDAOInstance.getPreviousCashBoxStatus(
-    "2021-01-10 02:51:55"
+    requiredDate
   );
+
+  let cashboxStatus = {
+    billete_100000: 0,
+    billete_50000: 0,
+    billete_20000: 0,
+    billete_10000: 0,
+    billete_5000: 0,
+    billete_1000: 0,
+    moneda_1000: 0,
+    moneda_500: 0,
+    moneda_200: 0,
+    moneda_100: 0,
+    moneda_50: 0,
+  };
+
+  let re_arranged_array = [];
+
+  for (let i = 0; i < result_body.length; i++) {
+    if (result_body[i].operations_id == 3) {
+      break;
+    } else {
+      re_arranged_array.push(result_body[i]);
+    }
+  }
+
+  for (let i = re_arranged_array.length - 1; i >= 0; i--) {
+    if (re_arranged_array[i].detail_operation_id == 1) {
+      cashboxStatus[re_arranged_array[i].denomination] += Number(
+        re_arranged_array[i].quantity
+      );
+    } else {
+      cashboxStatus[re_arranged_array[i].denomination] -= Number(
+        re_arranged_array[i].quantity
+      );
+    }
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({ results: result_body }),
