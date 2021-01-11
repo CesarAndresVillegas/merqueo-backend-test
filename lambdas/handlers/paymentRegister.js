6d48ea55b3f9d5478a85d97dd2c9660bc8b2e8c7
Cesar.Villegas.T <cesar@vendamasfacil.com>
2021-01-11 12:15:04 -0500

    Connection end on lambdas.

--- lambdas/handlers/paymentRegister.js
+++ lambdas/handlers/paymentRegister.js
@@ -72,7 +72,7 @@ exports.handler = async (event) => {
     };
   }
 
-  if (MySQLDAOInstance && MySQLDAOInstance.MySQLDAOInstance.connection) {
+  if (MySQLDAOInstance && MySQLDAOInstance.connection) {
     MySQLDAOInstance.connection.end();
   }
 
