a365cb40cc0d1d7e141f8e6b9d77ce7fa082d2b5
Cesar.Villegas.T <cesar@vendamasfacil.com>
2021-01-10 21:52:00 -0500

    Empty cashbox refactory.
    - Transaction modified to get logic out.
    - Query to get current denominations get out of transactions to be used by other resources.

--- lambdas/models/MySQLDAO.js
+++ lambdas/models/MySQLDAO.js
@@ -82,7 +82,8 @@ class MySQLDAO {
     });
   }
 
-  emptyCashBox() {
+  emptyCashBox(denominations) {
+    const size = denominations.length;
     let conn = this.connection;
     return new Promise((resolve, reject) => {
       conn.beginTransaction(function (err) {
@@ -90,11 +91,9 @@ class MySQLDAO {
           conn.end();
           reject(err);
         }
+
         conn.query(
-          `SELECT id, quantity
-          FROM cashbox
-          WHERE quantity > 0
-          ORDER BY id;`,
+          `INSERT INTO movements (payment, cash_back, operations_id) VALUES (0, 0, 3)`,
           function (error, results, fields) {
             if (error) {
               return conn.rollback(function () {
@@ -102,25 +101,10 @@ class MySQLDAO {
                 reject(error);
               });
             }
-            let denominations_to_register = { results: results };
-            const denominations = denominations_to_register.results;
-            const size = denominations.length;
+            let movement_id = results.insertId;
 
-            if (size == 0) {
-              return conn.rollback(function () {
-                conn.end();
-                reject("La caja se encuentra vacía");
-              });
-            }
-
-            let movement_params = {
-              payment: 0,
-              cash_back: 0,
-              operations_id: 3,
-            };
             conn.query(
-              `INSERT INTO movements SET ?`,
-              movement_params,
+              `UPDATE cashbox SET quantity = 0`,
               function (error, results, fields) {
                 if (error) {
                   return conn.rollback(function () {
@@ -128,51 +112,38 @@ class MySQLDAO {
                     reject(error);
                   });
                 }
-                let movement_id = results.insertId;
 
-                conn.query(
-                  `UPDATE cashbox SET quantity = 0`,
-                  function (error, results, fields) {
-                    if (error) {
-                      return conn.rollback(function () {
-                        conn.end();
-                        reject(error);
-                      });
-                    }
+                let insertQuery =
+                  "INSERT INTO movements_details (movements_id, cashbox_id, quantity, detail_operation_id) VALUES";
 
-                    let insertQuery =
-                      "INSERT INTO movements_details (movements_id, cashbox_id, quantity, detail_operation_id) VALUES";
+                let parameters = "";
 
-                    let parameters = "";
+                for (let i = 0; i < size; i++) {
+                  parameters += ` (${movement_id}, ${denominations[i].id}, ${denominations[i].quantity}, 2 ),`;
+                }
 
-                    for (let i = 0; i < size; i++) {
-                      parameters += ` (${movement_id}, ${denominations[i].id}, ${denominations[i].quantity}, 2 ),`;
-                    }
+                insertQuery += parameters;
+                insertQuery = insertQuery.replace(/.$/, ";");
 
-                    insertQuery += parameters;
-                    insertQuery = insertQuery.replace(/.$/, ";");
-
-                    conn.query(insertQuery, function (error, results, fields) {
-                      if (error) {
-                        return conn.rollback(function () {
-                          conn.end();
-                          reject(error);
-                        });
-                      }
-
-                      conn.commit(function (err) {
-                        if (err) {
-                          return conn.rollback(function () {
-                            conn.end();
-                            reject(err);
-                          });
-                        }
-                        conn.end();
-                        resolve("La caja se vació correctamente");
-                      });
+                conn.query(insertQuery, function (error, results, fields) {
+                  if (error) {
+                    return conn.rollback(function () {
+                      conn.end();
+                      reject(error);
                     });
                   }
-                );
+
+                  conn.commit(function (err) {
+                    if (err) {
+                      return conn.rollback(function () {
+                        conn.end();
+                        reject(err);
+                      });
+                    }
+                    conn.end();
+                    resolve("La caja se vació correctamente");
+                  });
+                });
               }
             );
           }
@@ -604,6 +575,22 @@ class MySQLDAO {
             body: JSON.stringify({ results: results }),
           };
           resolve(response);
+        }
+      );
+    });
+  }
+
+  getCurrentDenominations() {
+    let conn = this.connection;
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `SELECT id, quantity
+        FROM cashbox
+        WHERE quantity > 0
+        ORDER BY id;`,
+        function (error, results) {
+          if (error) reject(error);
+          resolve(results);
         }
       );
     });
